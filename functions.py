4130a14ec08c701c25a76158e074ae61b83cf1aa
Omiester <othnielamos9@gmail.com>
2026-02-18 00:31:38 +0100

    Finished the front end and started back end of /add

--- functions.py
+++ functions.py
@@ -1,6 +1,54 @@
 import sqlite3
-
+import json
 
 def get_db():
     return sqlite3.connect("students.db")
 
+def get_subjects():
+    with open("school.json","r") as file:
+        data = json.load(file)
+
+    list_subjects = data["subjects"]
+    return list_subjects
+
+def get_grades():
+    with open("school.json","r") as file:
+        data = json.load(file)
+
+    list_grades = data["grades"]
+    return list_grades
+
+def get_classes():
+    with open("school.json","r") as file:
+        data = json.load(file)
+
+    list_classes = data["classes"]
+    return list_classes
+
+def validate(fname,lname,og_grades,grade,og_subjects,subjects,og_classes,myclass):
+    try:
+        grade = int(grade)
+    except:
+        return False, "Invalid grade received"
+
+    if grade not in og_grades:
+        return False,"Invalid grade received"
+
+    if len(subjects) == 0:
+        return False,"No subject selected"
+    for subject in subjects:
+        if subject not in og_subjects or subject == "":
+            return False,"Invalid subject received"
+
+
+
+
+    if (not fname.isalpha()) or (not lname.isalpha()):
+        if fname == "" or lname == "":
+            return False, "A student's name can't be blank"
+        else:
+            return False,"Names should be only alphabetical"
+    if myclass not in og_classes:
+        return False,"Invalid class received"
+
+    return True,f"{fname} was added to the database"
--- main.py
+++ main.py
@@ -15,10 +15,6 @@ def login_needed(f):
         return f(*args, **kwargs)
     return wrapped_function
 
-
-
-
-
 @app.route("/")
 def temp():
     return redirect("/index")
@@ -72,4 +68,29 @@ def login():
 @login_needed
 def homepage():
     return render_template("homepage.html")
+
+@app.route("/add", methods=["GET","POST"])
+@login_needed
+def add():
+    og_subjects = get_subjects()
+    og_subjects.sort()
+    grades = get_grades()
+    classes = get_classes()
+
+    if request.method == "GET":
+        return render_template("add.html",subjects=og_subjects,grades=grades,classes=classes)
+    else:
+        fname = request.form.get("firstname").strip().title()
+        lname = request.form.get("lastname").strip().title()
+        grade = request.form.get("grade")
+        subjects = request.form.getlist("subjects")
+        myclass = request.form.get("class")
+        print("\n\n\n\n\n\n\n\n\n\n")
+        print(subjects)
+        valid, to_be_flashed = validate(fname,lname,grades,grade,og_subjects,subjects,classes,myclass)
+        flash(to_be_flashed)
+
+        return redirect("/add")
+
+
 
